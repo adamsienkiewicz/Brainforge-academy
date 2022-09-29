0e7e038efee126d5624d9a6bb5cd4b5900f90e9e
adamsienkiewicz <101409716+adamsienkiewicz@users.noreply.github.com>
2022-09-29 17:40:31 +0200

    ternary_operator

--- counter-app/src/App.js
+++ counter-app/src/App.js
@@ -3,7 +3,7 @@ import "./style.css";
 
 function App() {
   const [value, setValue] = useState(0)
-  // const [valueOn, setValueOn] = useState(0)
+  
   
 
   return (
@@ -12,12 +12,12 @@ function App() {
         <h1>prosty counter</h1>
       </div>
       <div>
-        <p className= { value < 1 ? 'red' : 'red' && value > 1 ? 'green':'green'}>{value}</p>  
+        <p className= {value <0 ? 'red' : 'green'}>{value}</p>  
       </div>
       <div class="btnStyle">
         <button className='btn' onClick={() => setValue(value +1)}>Increase</button>
-        <button className='btn' onClick={() => setValue(value -1)}>Decrease</button>
         <button className='btn' onClick={() => setValue(0)}>Reset</button>
+        <button className='btn' onClick={() => setValue(value -1)}>Decrease</button>
       </div>
     </div>
   );
